7600b4e6bcfee2f6f0d73d80b0b20e82faa32974
Lily Hopkins <lily@hpkns.uk>
2022-06-06 11:50:34 +0100

    Disabled spring context for testing

--- src/test/java/uk/hpkns/dvdrentalpos/POSApplicationTests.java
+++ src/test/java/uk/hpkns/dvdrentalpos/POSApplicationTests.java
@@ -3,7 +3,7 @@ package uk.hpkns.dvdrentalpos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class POSApplicationTests {
 
 	@Test

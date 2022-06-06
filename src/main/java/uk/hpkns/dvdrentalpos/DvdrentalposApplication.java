1e67025c4a05f2b8d898246013943c05bef682f7
Lily Hopkins <lily@hpkns.uk>
2022-06-06 09:47:22 +0100

    Initial commit

---
+++ src/main/java/uk/hpkns/dvdrentalpos/DvdrentalposApplication.java
@@ -0,0 +1,13 @@
+package uk.hpkns.dvdrentalpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdrentalposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdrentalposApplication.class, args);
+	}
+
+}

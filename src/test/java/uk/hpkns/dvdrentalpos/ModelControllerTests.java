70c895b2e8a9d6d70e92ee660a0ed6b8a3b20ab0
Lily Hopkins <lily@hpkns.uk>
2022-06-07 17:30:30 +0100

    Refactor code

--- src/test/java/uk/hpkns/dvdrentalpos/ModelControllerTests.java
+++ src/test/java/uk/hpkns/dvdrentalpos/ModelControllerTests.java
@@ -1,10 +1,8 @@
 package uk.hpkns.dvdrentalpos;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;

8bd060fe42a780332df08751f0c6dc29e0940ca1
Lily Hopkins <lily@hpkns.uk>
2022-06-23 21:21:10 +0100

    Improved ActorsController coverage

--- src/test/java/uk/hpkns/dvdrentalpos/api/v1/controllers/ActorsControllerTests.java
+++ src/test/java/uk/hpkns/dvdrentalpos/api/v1/controllers/ActorsControllerTests.java
@@ -1,11 +1,16 @@
 package uk.hpkns.dvdrentalpos.api.v1.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.hpkns.dvdrentalpos.api.v1.ResourceNotFoundException;
 import uk.hpkns.dvdrentalpos.data.models.Actor;
 import uk.hpkns.dvdrentalpos.data.repositories.ActorRepository;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class ActorsControllerTests extends ModelControllerTests<Actor, Integer, ActorRepository, ActorsController> {
 
@@ -43,5 +48,21 @@ public class ActorsControllerTests extends ModelControllerTests<Actor, Integer, 
         assertEquals(1, obj.getId(), "id incorrect");
         assertEquals("Amber", obj.getFirstName(), "first name incorrect");
         assertEquals("Heard", obj.getLastName(), "last name incorrect");
+    }
+
+    @Test
+    public void testSearch() {
+        controller.search("test");
+        verify(repository).findAllByFirstNameLikeOrLastNameLike("%test%", "%test%");
+    }
+
+    @Test
+    public void testGetFilms() {
+        Actor a = mock(Actor.class);
+        when(repository.findById(1)).thenReturn(Optional.of(a));
+        controller.getFilms(1);
+        verify(a).getFilms();
+
+        assertThrows(ResourceNotFoundException.class, () -> controller.getFilms(2));
     }
 }
